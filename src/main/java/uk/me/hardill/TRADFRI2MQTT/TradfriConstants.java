cc501fc14d2bf8477eba21f95869b13eb317850a
Lennart Buhl <git@hackmate.de>
2017-04-13 03:33:30 +0200

    Add color temperature constants

--- src/main/java/uk/me/hardill/TRADFRI2MQTT/TradfriConstants.java
+++ src/main/java/uk/me/hardill/TRADFRI2MQTT/TradfriConstants.java
@@ -14,20 +14,38 @@ public class TradfriConstants {
 	public static final int TYPE_BULB = 2;
 	// The others need to be figured out by people who own these
 
+
 	// Currently actually needed by Tradfri2MQTT
-	public static final String DEVICES = "15001"; // not included in Tradfri App IPSOObjects, but this needed everywhere
+	public static final String DEVICES = "15001"; // not included in IPSOObjects, but this is needed everywhere
 	public static final String GROUPS = "15004";
 
 	public static final String NAME = "9001"; // used in both devices and groups
-	public static final String INSTANCE_ID = "9003"; // internal ID of devices/groups
+	public static final String INSTANCE_ID = "9003"; // In devices: device ID. In groups: list of device IDs
 	public static final String HS_ACCESSORY_LINK = "9018";
 	public static final String LIGHT = "3311"; // urn:oma:lwm2m:ext:3311 in LwM2M registry
 
-	public static final String COLOR = "5706"; // not yet included, support will probably follow soon
 	public static final String TYPE = "5750"; // "Application Type" in LwM2M registry
 	public static final String ONOFF = "5850"; // "On/Off" in LwM2M registry
 	public static final String DIMMER = "5851"; // "Dimmer" in LwM2M registry
 	public static final String TRANSITION_TIME = "5712"; // not contained in LwM2M registry
+
+	// all three are different for each color, but fixed, independent of brightness
+	public static final String COLOR = "5706";
+	public static final String COLOR_X = "5709";
+	public static final String COLOR_Y = "5710";
+
+	// Not from Tradfri App, extracted from CoAP communication
+	public static final String COLOR_COLD = "f5faf6";
+	public static final String COLOR_COLD_X = "24930";
+	public static final String COLOR_COLD_Y = "24694";
+	public static final String COLOR_NORMAL = "f1e0b5";
+	public static final String COLOR_NORMAL_X = "30140";
+	public static final String COLOR_NORMAL_Y = "26909";
+	public static final String COLOR_WARM = "efd275";
+	public static final String COLOR_WARM_X = "33135";
+	public static final String COLOR_WARM_Y = "27211";
+
+
 
 	// Currently not needed, sorted alphabetically
 	public static final int DEFAULT_DIMMER_TRANSITION_TIME = 5;
@@ -47,8 +65,6 @@ public class TradfriConstants {
 	public static final int WAKE_UP_SMART_TASK = 3;
 	public static final String AUTH_PATH = "9063";
 	public static final String CLIENT_IDENTITY_PROPOSED = "9090";
-	public static final String COLOR_X = "5709";
-	public static final String COLOR_Y = "5710";
 	public static final String COMMISSIONING_MODE = "9061";
 	public static final String CREATED_AT = "9002";
 	public static final String CUM_ACTIVE_POWER = "5805";
