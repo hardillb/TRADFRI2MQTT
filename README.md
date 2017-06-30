0b578c1f8a9b40d25890a8901ed5c4bbbc9c9cef
Lennart Buhl <git@hackmate.de>
2017-06-30 22:33:06 +0200

    Code cleanup + Support for Mood control (#8)
    * Refactor & code cleanup
    
    Switched to id2name, added Apache Commons
    Added retained option
    Cleaner and prettier output (especially at startup)
    
    * Eclipse stuff doesn't belong in the repo
    
    * retained in README
    
    * Add support for Mood control

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Invocation
 
-java -jar TRADFRI2MQTT-X.X.X-SNAPSHOT.jar -ip [gateway ip] -psk [gateway secret] -broker [mqtt broker url]
+java -jar TRADFRI2MQTT-X.X.X-SNAPSHOT.jar -ip {gateway IP} -psk {gateway secret} -broker {MQTT broker URL} [-retained]
+
+The optional `-retained` configures the MQTT topics as retained.
 
 e.g.
 
@@ -13,7 +15,6 @@ Publishes state messages on topics like this:
  - TRÅDFRI/bulb/Living Room Light/state/temperature
  - TRÅDFRI/room/Living Room/state/on
  - TRÅDFRI/room/Living Room/state/dim
- 
 
 Subscribes to control messages on topics like this:
 
@@ -22,12 +23,19 @@ Subscribes to control messages on topics like this:
  - TRÅDFRI/bulb/Living Room Light/control/temperature
  - TRÅDFRI/room/Living Room/control/on
  - TRÅDFRI/room/Living Room/control/dim
+ - TRÅDFRI/room/Living Room/control/mood
 
 publish 0/1 to the `on` topic to turn the light off/on respectively
 
 publish 0-254 to the `dim` topic to change the brightness
 
-publish "cold" / "normal" / "warm" to the `temperature` topic to change temperatures. this only works on bulbs
+publish "cold" / "normal" / "warm" to the `temperature` topic to change temperatures.
+This only works on individual bulbs.
+
+publish the name of a mood (case-sensitive) to the `mood` topic of a room to adapt that mood.
+IKEA predefined moods are internally uppercase-only for some reason: "EVERYDAY" / "FOCUS" / "RELAX".
+Your self-defined moods have to be spelled like in the Trådfri App.
+At the moment, only control is implemented and state is not.
 
 # MQTT broker example
 An easy-to-use MQTT broker is [mosquitto](https://mosquitto.org/).
--- src/main/java/uk/me/hardill/TRADFRI2MQTT/TradfriConstants.java
+++ src/main/java/uk/me/hardill/TRADFRI2MQTT/TradfriConstants.java
@@ -9,23 +9,28 @@ package uk.me.hardill.TRADFRI2MQTT;
  */
 public class TradfriConstants {
 
-	// Device types (contained in INSTANCE_ID = "9003")
+	// Device types (contained in INSTANCE_ID = "9003" inside of array contained in LIGHT = "3311")
 	public static final int TYPE_REMOTE = 0;
 	public static final int TYPE_BULB = 2;
 	// The others need to be figured out by people who own these
 
 	// Top level navigation
-	public static final String DEVICES = "15001";
-	public static final String GROUPS = "15004";
+	public static final String DEVICES = "15001"; // individual bulbs, remotes, motion sensors, ...
+	public static final String GROUPS = "15004"; // also called rooms
+	public static final String SCENE = "15005"; // also called moods
+	public static final String NOTIFICATIONS = "15006"; // ???
+	public static final String TIMERS = "15010";
 
 	// Values in JSON data
 	public static final String NAME = "9001"; // used in both devices and groups
 	public static final String INSTANCE_ID = "9003"; // In devices: device ID. In groups: list of device IDs
-	public static final String HS_ACCESSORY_LINK = "9018";
+	public static final String HS_ACCESSORY_LINK = "9018"; // Groups have this entry
+	public static final String IKEA_MOODS = "9068"; // Moods have this entry
 	public static final String LIGHT = "3311"; // urn:oma:lwm2m:ext:3311 in LwM2M registry
 	public static final String TYPE = "5750"; // "Application Type" in LwM2M registry
 	public static final String ONOFF = "5850"; // "On/Off" in LwM2M registry
 	public static final String DIMMER = "5851"; // "Dimmer" in LwM2M registry
+	public static final String SCENE_ID = "9039"; // Property of rooms
 	public static final String TRANSITION_TIME = "5712"; // not contained in LwM2M registry
 
 	// Color / Temperature related, these are independent of brightness, i.e. do not change if brightness does
